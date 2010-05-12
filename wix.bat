b9fb86caa05a4813fd7fc956b8c7b3f3fc895c16
little.mole <little.mole@arcor.de>
2010-05-12 13:46:51 +0000

    re structure build

---
+++ wix.bat
@@ -0,0 +1,95 @@
+rem build both admin and non-privilegded user installers
+rem ----------------------------------------------------
+
+set MODE=%1
+set MODE=%MODE:"=%
+
+set MSI=%2
+set MSI=%MSI:"=%
+
+set PLATFORM=%3
+
+set MSILIGHT=%MSI:"=%light.msi
+set MSI=%MSI%.msi
+
+set TARGET=%4
+if NOT "%TARGET%"=="/clean" goto compile
+
+if "%TARGET%"=="/clean" goto clean
+
+goto end
+
+
+
+:clean
+
+rem ---clean--------------------------------------------
+
+
+cd bin/WixMoe
+
+SET DEST="deploy\%MODE%\%MSILIGHT:"=%"
+del %DEST%
+
+SET DEST="deploy\%MODE%\%MSI%"
+del %DEST%
+
+goto end
+
+
+
+:compile
+
+rem ---build-msi----------------------------------------
+
+
+cd bin/WixMoe
+
+call wix.bat "%MODE%" %MSI% %PLATFORM%
+
+IF ERRORLEVEL 1 GOTO hell
+
+
+rem ---non priviledged user-----------------------------
+
+
+SET SRC="bin\%MODE%\%MSI:"=%"
+SET DEST="deploy\%MODE%\%MSILIGHT:"=%"
+
+mkdir "deploy\%MODE%"
+copy /Y %SRC% %DEST%
+
+cscript "moeEditMSI.wsf" %DEST%
+
+IF ERRORLEVEL 1 GOTO hell
+
+
+
+rem ---admin install------------------------------------
+
+
+SET DEST="deploy\%MODE%\%MSI%"
+
+copy /Y %SRC% %DEST%
+
+
+rem ----------------------------------------------------
+
+
+goto end
+
+
+
+:hell
+
+cd ..
+cd ..
+
+exit /B 1
+
+:end
+
+cd ..
+cd ..
+
+exit /B 0
