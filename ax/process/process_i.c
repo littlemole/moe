116398696969b2cd7d8183602f0817fe5eff4c69
little.mole <little.mole@arcor.de>
2010-05-24 16:06:11 +0000

    nb64 success

--- ax/process/process_i.c
+++ ax/process/process_i.c
@@ -6,10 +6,10 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon May 24 16:59:12 2010
+/* at Mon May 24 17:45:21 2010
  */
 /* Compiler settings for .\process.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
