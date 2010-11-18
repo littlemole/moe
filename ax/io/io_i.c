0c062e307d6b6fd73cea13fc9afa48d006f608cf
little.mole <little.mole@arcor.de>
2010-11-18 17:21:12 +0000

    cleanup

--- ax/io/io_i.c
+++ ax/io/io_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Nov 02 22:41:04 2010
+/* at Sat Nov 13 12:56:30 2010
  */
 /* Compiler settings for io.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
