60b9c71be50a13e4379db8128ce2f0a26253ed9c
little.mole <little.mole@arcor.de>
2010-11-18 17:20:28 +0000

    cleanup

--- ax/comtypes/stdafx.h
+++ ax/comtypes/stdafx.h
@@ -1,37 +1,28 @@
-// stdafx.h : Includedatei für Standardsystem-Includedateien
-// oder häufig verwendete projektspezifische Includedateien,
-// die nur in unregelmäßigen Abständen geändert werden.
-//
-
 #pragma once
-/*
-// Ändern Sie folgende Definitionen für Plattformen, die älter als die unten angegebenen sind.
-// In MSDN finden Sie die neuesten Informationen über die entsprechenden Werte für die unterschiedlichen Plattformen.
-#ifndef WINVER				// Lassen Sie die Verwendung spezifischer Features von Windows XP oder später zu.
-#define WINVER 0x0501		// Ändern Sie dies in den geeigneten Wert für andere Versionen von Windows.
-#endif
 
-#ifndef _WIN32_WINNT		// Lassen Sie die Verwendung spezifischer Features von Windows XP oder später zu.                   
-#define _WIN32_WINNT 0x0501	// Ändern Sie dies in den geeigneten Wert für andere Versionen von Windows.
-#endif						
+#include "ole/typeinfo.h"
+#include "ole/ole.h"
+#include "ole/com.h"
+#include "ole/obj.h"
+#include "ole/Bstr.h"
+#include "ole/typeinfo.h"
+#include "ole/Factory.h"
+#include "ole/aut.h"
+#include "ole/cp.h"
+#include "win/path.h"
+#include "win/file.h"
+#include "util/istr.h"
+#include "thread/thread.h"
+#include <msxml2.h>
+#include <list>
+#include <map>
+#include <set>
+#include <vector>
 
-#ifndef _WIN32_WINDOWS		// Lassen Sie die Verwendung spezifischer Features von Windows 98 oder später zu.
-#define _WIN32_WINDOWS 0x0410 // Ändern Sie dies in den geeigneten Wert für Windows Me oder höher.
-#endif
+#define BOOST_BIND_ENABLE_STDCALL 
+#include "boost/bind.hpp"
 
-#ifndef _WIN32_IE			// Lassen Sie die Verwendung spezifischer Features von IE 6.0 oder später zu.
-#define _WIN32_IE 0x0600	// Ändern Sie dies in den geeigneten Wert für andere Versionen von IE.
-#endif
+#include "comtypes_h.h"
 
-#define WIN32_LEAN_AND_MEAN		// Selten verwendete Teile der Windows-Header nicht einbinden.
-// Windows-Headerdateien:
-#include <windows.h>
-
-// C RunTime-Headerdateien
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
-
-*/
-// TODO: Hier auf zusätzliche Header, die das Programm erfordert, verweisen.
+#include "Meta.h"
+#include "types.h"
