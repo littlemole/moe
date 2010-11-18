b7c1fcf6a25285930eee3dbfd59315c8cf916294
little.mole <little.mole@arcor.de>
2010-11-18 17:20:01 +0000

    cleanup

--- lib/src/ole/HtmlEvents_h.h
+++ lib/src/ole/HtmlEvents_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Nov 02 22:40:01 2010
+/* at Sat Nov 13 12:55:27 2010
  */
 /* Compiler settings for HtmlEvents.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
