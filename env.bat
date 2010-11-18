feee1c7935c8f8dba10f855cb8a25a5e91ed2de9
little.mole <little.mole@arcor.de>
2010-11-18 17:41:03 +0000

    cleanup

---
+++ env.bat
@@ -0,0 +1,22 @@
+set CONF=%1
+if %1!==! SET CONF=uni_debug
+
+set PLAT=%2
+if NOT %2!==! set PLAT=/x86
+
+if "%CONF%"=="uni_debug" goto debug
+
+set CONFIG=/Release
+goto psdk
+
+:debug
+set CONFIG=/Debug
+
+
+:psdk
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\bin\SetEnv.Cmd" %CONFIG% %PLAT%
+
+set Configuration=%CONF%
+
+echo Configuration: %Configuration%
+exit /B
