79ccbfd461781887d420010440e56e412092a9d0
little.mole <little.mole@arcor.de>
2011-02-26 23:26:04 +0000

    msbuild for wixmoe

--- ax/io/io_i.c
+++ ax/io/io_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Feb 26 18:22:17 2011
+/* at Sun Feb 27 00:04:49 2011
  */
 /* Compiler settings for io.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
