c7f6ec8c847c8bc807ea27f34a2715e4c28780c0
little.mole <little.mole@arcor.de>
2011-02-28 19:52:20 +0000

    cygpath support

--- ax/jre/jre_h.h
+++ ax/jre/jre_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Feb 28 20:27:03 2011
+/* at Mon Feb 28 20:51:35 2011
  */
 /* Compiler settings for jre.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
