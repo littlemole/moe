e23b023b79bf08c887160b9c26a73fc1a9a40231
little.mole <little.mole@arcor.de>
2011-02-28 19:51:14 +0000

    cygpath support

--- ax/comtypes/comtypes_i.c
+++ ax/comtypes/comtypes_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Feb 28 20:19:24 2011
+/* at Mon Feb 28 20:40:07 2011
  */
 /* Compiler settings for comtypes.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
