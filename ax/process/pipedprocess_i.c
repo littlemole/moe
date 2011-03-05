6afa7fd5cb84bcc53f9f254ecd444a523c3dc9fc
little.mole <little.mole@arcor.de>
2011-03-05 22:14:13 +0000

    wip

--- ax/process/pipedprocess_i.c
+++ ax/process/pipedprocess_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Mar 05 22:46:00 2011
+/* at Sat Mar 05 23:06:23 2011
  */
 /* Compiler settings for process.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
--- lib/include/java/java.h
+++ lib/include/java/java.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Mar 02 22:48:09 2011
+/* at Sat Mar 05 23:10:57 2011
  */
 /* Compiler settings for java.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
