5d53430d47c1e1d6e9d83f36478c572b88fb0a2c
little.mole <little.mole@arcor.de>
2011-03-06 11:23:01 +0000

    wip

--- ax/io/io_i.c
+++ ax/io/io_i.c
@@ -6,10 +6,10 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Mar 05 23:06:15 2011
+/* at Sat Mar 05 23:17:43 2011
  */
 /* Compiler settings for io.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
