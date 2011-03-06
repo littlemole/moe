19d912706622b9b2dff2cb0ba499b3f10517469f
little.mole <little.mole@arcor.de>
2011-03-06 11:23:01 +0000

    wip

--- ax/jre/jre_i.c
+++ ax/jre/jre_i.c
@@ -6,10 +6,10 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Mar 05 23:11:08 2011
+/* at Sun Mar 06 12:12:56 2011
  */
 /* Compiler settings for jre.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
