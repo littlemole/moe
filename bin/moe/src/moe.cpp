4a6650c2e4cb64ea6746115e0880e4a8b337b6df
little.mole <little.mole@arcor.de>
2011-07-03 22:26:10 +0000

    prepare backup

--- bin/moe/src/moe.cpp
+++ bin/moe/src/moe.cpp
@@ -571,7 +571,6 @@ void MoeWnd::OnFx(int code, int id, HWND ctrl)
 	int fx =id-IDM_F1+1;
 
 	mol::ostringstream oss;
-	//oss << _T("\\fx\\F") << fx << _T(".js");
 	oss << _T("fx\\F") << fx << _T(".js");
 
 	mol::string file = findFile(oss.str());
