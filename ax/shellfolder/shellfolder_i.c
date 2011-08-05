96e262278ac92969c2ad01842afcc4fcf4776521
little.mole <little.mole@arcor.de>
2011-08-05 23:07:33 +0000

    release with new shelltree and new ribbon

--- ax/shellfolder/shellfolder_i.c
+++ ax/shellfolder/shellfolder_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Aug 05 00:58:41 2011
+/* at Sat Aug 06 01:02:09 2011
  */
 /* Compiler settings for shellfolder.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
