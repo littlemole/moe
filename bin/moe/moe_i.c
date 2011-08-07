a4d8298347fdf72de98bd6fa7daaa2b2296c01b3
little.mole <little.mole@arcor.de>
2011-08-07 20:27:32 +0000

    dirview renaming

--- bin/moe/moe_i.c
+++ bin/moe/moe_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Aug 07 02:37:49 2011
+/* at Sun Aug 07 22:18:41 2011
  */
 /* Compiler settings for src\moe.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
--- lib/src/ole/HtmlEvents_h.h
+++ lib/src/ole/HtmlEvents_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Aug 07 02:35:26 2011
+/* at Sun Aug 07 22:16:04 2011
  */
 /* Compiler settings for HtmlEvents.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
