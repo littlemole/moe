cee265423485e1b98fb5c21f194728cdc222fec2
little.mole <little.mole@arcor.de>
2011-08-14 17:28:28 +0000

    full build

--- ax/jre/jre_i.c
+++ ax/jre/jre_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Mar 20 20:49:43 2011
+/* at Sun Aug 14 16:58:22 2011
  */
 /* Compiler settings for jre.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
