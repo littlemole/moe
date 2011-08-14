a84b5c86adc6219a1594267683cb6d812a827635
little.mole <little.mole@arcor.de>
2011-08-14 20:19:56 +0000

    build consistency check

--- ax/shellctrl/shellctrl_h.h
+++ ax/shellctrl/shellctrl_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Aug 14 15:12:15 2011
+/* at Sun Aug 14 21:38:29 2011
  */
 /* Compiler settings for shellCtrl.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
