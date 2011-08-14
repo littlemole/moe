996cf8282227d5754fb0d52165b5e3b64fff4757
little.mole <little.mole@arcor.de>
2011-08-14 17:28:28 +0000

    full build

--- lib/include/java/java.h
+++ lib/include/java/java.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Aug 07 00:24:00 2011
+/* at Sun Aug 14 16:58:10 2011
  */
 /* Compiler settings for java.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
