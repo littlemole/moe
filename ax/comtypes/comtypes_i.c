c17886b617787c2282f5195d0cb13add485d4222
little.mole <little.mole@arcor.de>
2011-08-20 18:03:56 +0000

    cleanup

--- ax/comtypes/comtypes_i.c
+++ ax/comtypes/comtypes_i.c
@@ -6,7 +6,11 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
+<<<<<<< .mine
+/* at Sun Aug 14 20:51:46 2011
+=======
 /* at Sun Aug 14 21:37:52 2011
+>>>>>>> .r386
  */
 /* Compiler settings for comtypes.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
