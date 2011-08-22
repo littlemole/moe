85a3d24ca38668a769f4378160f9a67c6cb7ad7a
little.mole <little.mole@arcor.de>
2011-08-22 21:51:56 +0000

    replace fix for scintilla editor

--- lib/src/ole/HtmlEvents_i.c
+++ lib/src/ole/HtmlEvents_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Aug 21 21:35:59 2011
+/* at Mon Aug 22 23:39:07 2011
  */
 /* Compiler settings for HtmlEvents.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
