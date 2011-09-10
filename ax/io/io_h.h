3b77df5416c4bc6819670cc5ac0f67a40f501ccd
little.mole <little.mole@arcor.de>
2011-09-10 16:41:44 +0000

    improved buildsystem and fixes

--- ax/io/io_h.h
+++ ax/io/io_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Sep 05 23:13:34 2011
+/* at Sat Sep 10 16:50:15 2011
  */
 /* Compiler settings for io.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
