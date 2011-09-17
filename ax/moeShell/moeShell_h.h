c57893005387d125864f470c0faa96c9aa9c456d
little.mole <little.mole@arcor.de>
2011-09-17 11:50:37 +0000

    cleanup build

--- ax/moeShell/moeShell_h.h
+++ ax/moeShell/moeShell_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Sep 12 21:50:27 2011
+/* at Sat Sep 17 13:34:08 2011
  */
 /* Compiler settings for moeShell.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
