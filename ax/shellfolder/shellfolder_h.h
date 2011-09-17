44e453b96e96ae4b23e8d660898b3e06fce923c9
little.mole <little.mole@arcor.de>
2011-09-17 14:19:14 +0000

    new build

--- ax/shellfolder/shellfolder_h.h
+++ ax/shellfolder/shellfolder_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Sep 17 13:32:09 2011
+/* at Sat Sep 17 15:50:09 2011
  */
 /* Compiler settings for shellfolder.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
