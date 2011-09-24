94338912d5ba2b1f8f2eba149476cde1245b19a0
little.mole <little.mole@arcor.de>
2011-09-24 08:43:42 +0000

    bugfixin winxp mode - menus

--- ax/moeShell/moeShell_i.c
+++ ax/moeShell/moeShell_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Sep 22 22:44:04 2011
+/* at Sat Sep 24 00:55:31 2011
  */
 /* Compiler settings for moeShell.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
--- ax/setting/setting_h.h
+++ ax/setting/setting_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Sep 22 22:41:18 2011
+/* at Sat Sep 24 00:52:45 2011
  */
 /* Compiler settings for setting.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
