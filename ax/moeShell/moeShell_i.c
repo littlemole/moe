1e621dbe59fcdbaa6e4ad030a99785f42b483f16
little.mole <little.mole@arcor.de>
2011-10-11 18:07:33 +0000

    baseline

--- ax/moeShell/moeShell_i.c
+++ ax/moeShell/moeShell_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Oct 04 00:26:05 2011
+/* at Sun Oct 09 23:07:47 2011
  */
 /* Compiler settings for moeShell.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
--- ax/scintillax/ScintillAx_h.h
+++ ax/scintillax/ScintillAx_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Oct 04 00:46:01 2011
+/* at Sun Oct 09 23:06:18 2011
  */
 /* Compiler settings for ScintillAX.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
