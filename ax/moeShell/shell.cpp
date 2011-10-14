3a503394a980d3e3c93157eb3c82ced3ea4a2551
little.mole <little.mole@arcor.de>
2011-10-14 16:47:25 +0000

    move

--- ax/moeShell/shell.cpp
+++ ax/moeShell/shell.cpp
@@ -8,7 +8,11 @@
 #include "moeShell_i.c"
 #include "moe_i.c"
 
+#if defined _M_X64
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else 
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
 
 ///////////////////////////////////////////////////////////////////////////////
 
--- lib/src/ole/HtmlEvents_i.c
+++ lib/src/ole/HtmlEvents_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Oct 09 23:05:10 2011
+/* at Fri Oct 14 00:53:35 2011
  */
 /* Compiler settings for HtmlEvents.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
