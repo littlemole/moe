36dd18592ce3c7601fe7fe224078c294ecd15942
little.mole <little.mole@arcor.de>
2011-11-20 17:43:21 +0000

    even more ssh stuff

--- ax/setting/setting_i.c
+++ ax/setting/setting_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Nov 19 18:41:33 2011
+/* at Sun Nov 20 16:54:18 2011
  */
 /* Compiler settings for setting.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
--- bin/moe/moe_i.c
+++ bin/moe/moe_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sat Nov 19 18:43:35 2011
+/* at Sun Nov 20 16:56:33 2011
  */
 /* Compiler settings for src\moe.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
