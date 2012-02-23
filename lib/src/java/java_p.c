0cd7454ed4f5d00a7e2d1a86758094fa8126a490
U-janet\mike <mike@janet.(none)>
2012-02-23 21:21:42 +0100

    ssh fix etc

--- lib/src/java/java_p.c
+++ lib/src/java/java_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Dec 12 23:10:39 2011
+/* at Thu Feb 23 19:29:17 2012
  */
 /* Compiler settings for java.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
