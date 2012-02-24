a956f08d278ce2f14a22861ba7dc1985d6167e3c
U-janet\mike <mike@janet.(none)>
2012-02-24 21:31:27 +0100

    new date strategy for bat file

--- release.bat
+++ release.bat
@@ -4,7 +4,23 @@ SET SRC=%1
 SET TARGET=%2
 SET MODE=%3
 
-SET TODAY=%date:~-4%-%date:~-7,-5%-%date:~0,2%
+set year=%date:~-4%
+rem echo year=%year%
+
+set month=%date:~-7,-5%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+rem echo month=%month%
+
+set day=%date:~0,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+rem echo day=%day%
+
+set TODAY=%year%%month%%day%
+echo %TODAY%
+
+rem SET TODAY=%date:~-4%-%date:~-7,-5%-%date:~0,2%
+
+
 SET REMOTE_DIR=/srv/www/htdocs/oha7/moe/%TODAY%/
 set SCP="C:\Program Files (x86)\PuTTY\pscp.exe" -i "C:\Users\mike\putty2betty.ppk"
 set RC="C:\Program Files (x86)\PuTTY\plink.exe" -i "C:\Users\mike\putty2betty.ppk" mike@knitbits.de
