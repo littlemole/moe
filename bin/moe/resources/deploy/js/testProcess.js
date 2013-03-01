642bbc3732dac4e0d3cec7397c969ae2a68dfec2
U-janet\mike <mike@janet.(none)>
2013-03-01 20:31:26 +0100

    fix build on fresh checkout (missing scintilla custom out directories)

--- bin/moe/resources/deploy/js/testProcess.js
+++ bin/moe/resources/deploy/js/testProcess.js
@@ -2,17 +2,20 @@ var killRoy = new ActiveXObject("ShellCtrls.KillRoy");
 
 if(killRoy)
 {
-  var file = Dialogs.ChooseFile();
+  var file = moe.Dialogs.ChooseFile();
   if (file)
   {
 	var pid = killRoy.FindPIDforFile(file);
-	var result = Dialogs.MsgBox(
+	if(pid)
+	{
+	var result = moe.Dialogs.MsgBox(
 		"the pid of the process holding\r\n\r\n" + file +"\r\n\r\nis: " + pid + " " + pid.Name, 
-		"kill the process?",Dialogs.YESNO|Dialogs.ICONQUESTION
+		"kill the process?",moe.Dialogs.YESNO|moe.Dialogs.ICONQUESTION
 		);
-	if ( result == IDYES ) 
+	if ( result == moe.IDYES ) 
 	{
 	  pid.TerminateProcess();
+	}
 	}
   }
 }
