4b3922abb9d12cca48ea49f3af8edeb0069c66fc
U-janet\mike <mike@janet.(none)>
2013-08-07 21:04:56 +0200

    first build vc2013

---
+++ env12.bat
@@ -0,0 +1,39 @@
+@echo off
+
+set PlatformTools=v120
+set UICC="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\uicc.exe"
+
+set CONF=%1
+if %1!==! SET CONF=uni_debug
+
+set PLAT=%2
+if %2!==! set PLAT=x86
+
+if "%CONF%"=="uni_debug" goto debug
+
+set CONFIG=/Release
+goto psdk
+
+:debug
+set CONFIG=/Debug
+
+
+:psdk
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" %PLAT%
+
+set Configuration=%CONF%
+
+if "%PLAT%"=="x86" goto x86
+set Platform=x64
+goto end
+
+:x86
+set Platform=Win32
+
+:end
+
+echo Configuration: %Configuration%
+echo Platform: %Platform%
+echo %PLAT%
+echo %PlatformTools%
+exit /B
