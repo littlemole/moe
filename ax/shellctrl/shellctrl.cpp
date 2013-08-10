5b56bed7e7e346e10a6d18aa3115eba05732902a
U-janet\mike <mike@janet.(none)>
2013-08-10 16:00:27 +0200

    fix lost shellctrl.cpp

---
+++ ax/shellctrl/shellctrl.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "ole/Dll.h"
+
+#include "ColorPickerDlg.h"
+#include "ShellTreeCtrl.h"
+#include "NamespaceTree.h"
+#include "ShellListCtrl.h"
+#include "ScpListCtrl.h"
+#include "HexCtrl.h"
+#include "Killroy.h"
+
+#include "ShellCtrl_i.c"
+#include "../ssh/ssh_i.c"
+
+class shellCtrlDll : 
+	public Dll,
+	public exports_aggregable< shellCtrlDll, ShellTree >,
+	public exports_aggregable< shellCtrlDll, ShellListCtrl >,
+	public exports_aggregable< shellCtrlDll, NamespaceTree >,
+	public exports_aggregable< shellCtrlDll, ColorDlg >,
+	public exports_aggregable< shellCtrlDll, HexCtrl >,
+	public exports_aggregable< shellCtrlDll, ScpListCtrl >,
+	public exports_aggregable< shellCtrlDll, KillRoy >
+{};
+
+DLL_COCLASS_EXPORTS(shellCtrlDll);
