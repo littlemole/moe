c07faa5be0fde9ae45036fc7d8860e95eedf7f24
mike <mike@oha7.org>
2017-04-16 20:42:13 +0200

    first push vc2017

--- ax/userform/userforms_h.h
+++ ax/userform/userforms_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sun Apr 16 00:53:20 2017
+/* at Sun Apr 16 19:36:03 2017
  */
 /* Compiler settings for userform.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
